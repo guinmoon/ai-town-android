56e845f4495cce54a8f37d7a48085520d7a8e837
guinmoon <guinmoon@gmail.com>
2025-09-03 18:58:18 +0300

    update insall script

--- install_termux.sh
+++ install_termux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Updating package list and upgrading installed packages"
-apt update && apt upgrade
+apt update && apt upgrade -y
 
 echo "Installing required dependencies: clang, wget, just, python, cmake, libcurl, git, screen, proot-distro, nodejs-lTS"
-apt install clang wget just python cmake libcurl git screen proot-distro nodejs-lts
+apt install clang wget just python cmake libcurl git screen proot-distro nodejs-lts -y
 
 echo "Installing Debian Linux distribution via proot-distro"
 proot-distro install debian
